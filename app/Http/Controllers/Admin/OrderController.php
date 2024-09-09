dcceb037547baf624d4bdcf416dd1074f2b7259e
Manzar <manzar@Manzars-MacBook-Pro.local>
2024-09-09 17:14:36 +0500

    wholesale_status in admin panel

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -72,7 +72,8 @@ class OrderController extends Controller
             if ($status === 'wholesale') {
                 $query->where(['is_wholesale' => 1]);
             } elseif ($status === 'pending') {
-                $query->where(['order_status' => 'pending']);
+                $qry = $query->where(['order_status' => 'pending'])->get();
+                dd($qry);
             } elseif ($status !== 'all') {
                 $query->where(['order_status' => $status]);
             }
