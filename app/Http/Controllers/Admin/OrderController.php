8d116130eca6ec6b745a6574c96b3aaf3aa34607
Manzar <manzar@Manzars-MacBook-Pro.local>
2024-09-09 13:42:51 +0500

    wholesale_status in admin panel

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -72,8 +72,8 @@ class OrderController extends Controller
         });
 
     if ($status === 'wholesale') {
-        $fdata = $query->where('is_wholesale', 1)->get();
-        dd($fdata);
+            $query->where('is_wholesale', 1)->get();
+        
     } elseif ($status != 'all') {
         $query->where(['order_status' => $status]);
     }
