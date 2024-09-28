c78d5e3051eda028542f8e9f871795b6a1a38911
“manzarnazar” <manzarnazar@gmail.com>
2024-09-28 14:30:38 +0400

    Your commit message

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2542,4 +2542,5 @@
   'Turning Collaboration will create offer on product Collaboration' => 'Turning Collaboration will create offer on product Collaboration',
   'Last date is required for collaboration products!' => 'Last date is required for collaboration products!',
   'Collaboration note is required if the product is Collaboration!' => 'Collaboration note is required if the product is Collaboration!',
+  'Waitlist' => 'Waitlist',
 );
